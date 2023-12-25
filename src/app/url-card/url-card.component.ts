38ede669ef4ff93df8519cd05e4220997e56d43a
waglarry <emmanuelacquaah2468@gmail.com>
2023-12-25 23:21:42 +0000

    Create UrlCard component

---
+++ src/app/url-card/url-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './url-card.component.html',
+  styleUrl: './url-card.component.css'
+})
+export class UrlCardComponent {
+
+}

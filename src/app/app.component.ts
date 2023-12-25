0718c3d36264dcfd3889cad31df41537e5f78b5c
waglarry <emmanuelacquaah2468@gmail.com>
2023-12-25 23:07:07 +0000

    Update dashboard component with header UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DashboardComponent, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgTinyUrlService } from 'ng-tiny-url';
+import { ShortenedUrlsService } from '../services/shortened-urls.service';
 
+interface URLData {
+  title: string;
+  shortenedUrl: string;
+  inputUrl: string;
+  date: string;
+  urlLogo: string;
+}
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
   title = 'Shortingo';
+  brandImageUrl: string = 'assets/images/Shortingo.svg';
+  linkIconUrl: string = 'assets/images/link.svg';
 
+  date: string = new Date().toISOString();
+  isFormSubmitted = false;
+  isLoading = false;
+  isTextCopied = false;
+  shortenedUrl = '';
+
+  model: URLData = {
+    title: `Untitled ${this.date}`,
+    shortenedUrl: '',
+    inputUrl: '',
+    date: new Date().toDateString(),
+    urlLogo: '',
+  };
+
+  urlsData: Array<object> = [];
+
+  constructor(
+    private _ngTinyUrlService: NgTinyUrlService,
+    private _saveUrl: ShortenedUrlsService
+  ) {}
+
+  handleOnSubmit() {}
 }

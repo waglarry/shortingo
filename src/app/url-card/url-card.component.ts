bff89eb7604e83f4b0c3a58e32800f924151f23b
waglarry <emmanuelacquaah2468@gmail.com>
2023-12-26 01:09:13 +0000

    Add delete function

--- src/app/url-card/url-card.component.ts
+++ src/app/url-card/url-card.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-url-card',
   standalone: true,
   imports: [],
   templateUrl: './url-card.component.html',
-  styleUrl: './url-card.component.css'
+  styleUrl: './url-card.component.css',
 })
 export class UrlCardComponent {
-
+  constructor(public dashboardData: DashboardComponent) {}
 }

aee6eaea8b4fb282af0b0a57a87ffa4e90a40d66
waglarry <emmanuelacquaah2468@gmail.com>
2023-12-27 22:06:29 +0000

    Add routes and models to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule, DashboardComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HttpClientModule,
+    DashboardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'shortingo';
-}
+export class AppComponent {}

018653f2d9e1bf1c5949dadde296c95c3d84d69e
waglarry <emmanuelacquaah2468@gmail.com>
2024-01-08 20:45:30 +0000

    Create verify-otp component

---
+++ src/app/components/verifyotp/verifyotp.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerifyotpComponent } from './verifyotp.component';
+
+describe('VerifyotpComponent', () => {
+  let component: VerifyotpComponent;
+  let fixture: ComponentFixture<VerifyotpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [VerifyotpComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(VerifyotpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/verifyotp/verifyotp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verifyotp',
+  standalone: true,
+  imports: [],
+  templateUrl: './verifyotp.component.html',
+  styleUrl: './verifyotp.component.css'
+})
+export class VerifyotpComponent {
+
+}

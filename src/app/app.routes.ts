feed7e5af6356089490b68943b8198949545f3bc
waglarry <emmanuelacquaah2468@gmail.com>
2024-01-08 18:52:46 +0000

    Add home and dashboard routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { authGuard } from './guard/auth.guard';
+import { HomepageComponent } from './homepage/homepage/homepage.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: HomepageComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+  },
 ];

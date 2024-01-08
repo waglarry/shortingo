d1f4f601247710e4f7ce7c6986b2d15a1812e114
waglarry <emmanuelacquaah2468@gmail.com>
2024-01-08 19:05:17 +0000

    Update shortened-url service with backend enpoints

--- src/app/services/shortened-urls.service.ts
+++ src/app/services/shortened-urls.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -6,25 +6,45 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ShortenedUrlsService {
-  endpoint = 'http://localhost:3000/shortened-urls';
+  baseUrl = 'https://shortingo-api.onrender.com/api';
+  authToken = sessionStorage.getItem('token');
 
   constructor(private _http: HttpClient) {}
 
-  saveUrlToDatebase(updatedPropertyValue: any): Observable<any> {
-    return this._http.post<any>(this.endpoint, updatedPropertyValue);
-  }
+  headers = new HttpHeaders({
+    Authorization: `Bearer ${this.authToken}`,
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  });
 
-  getSavedUrls(searchTerm?: string): Observable<any> {
+  getAllUserUrls(userId: string, searchTerm?: string): Observable<any> {
     return this._http.get<object>(
-      `${this.endpoint}${searchTerm ? '?q=' + searchTerm : '/'}`
+      `${this.baseUrl}${searchTerm ? '/search/?title=' : '/get-all/'}${
+        searchTerm ? searchTerm : userId
+      }`,
+      {
+        headers: this.headers,
+      }
     );
   }
 
-  deleteUrl(id: number): Observable<any> {
-    return this._http.delete<object>(`${this.endpoint}/${id}`);
+  saveUrl(data: any): Observable<any> {
+    return this._http.post<any>(`${this.baseUrl}/create`, data, {
+      headers: this.headers,
+    });
   }
 
-  updateUrl(id: number, data: object): Observable<any> {
-    return this._http.patch<object>(`${this.endpoint}/${id}`, data);
+  deleteUrl(id: string): Observable<any> {
+    return this._http.delete<object>(`${this.baseUrl}/delete/${id}`, {
+      headers: this.headers,
+    });
   }
+
+  updateUrl(id: string, data: object): Observable<any> {
+    return this._http.patch<object>(`${this.baseUrl}/update/${id}`, data, {
+      headers: this.headers,
+    });
+  }
+
 }

64c576b56f43978973e493a1c8b0c4259ddae847
waglarry <emmanuelacquaah2468@gmail.com>
2024-01-13 19:33:52 +0000

    Design verifyotp UI

--- src/app/components/verifyotp/verifyotp.component.ts
+++ src/app/components/verifyotp/verifyotp.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-verifyotp',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './verifyotp.component.html',
   styleUrl: './verifyotp.component.css'
 })

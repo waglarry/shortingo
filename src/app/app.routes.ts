bae978f034dd4f5ffb7b789792dc575b3e4857ca
waglarry <emmanuelacquaah2468@gmail.com>
2024-01-13 19:34:05 +0000

    Add new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,17 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './guard/auth.guard';
 import { HomepageComponent } from './homepage/homepage/homepage.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { VerifyotpComponent } from './components/verifyotp/verifyotp.component';
+import { CreatenewpasswordComponent } from './components/createnewpassword/createnewpassword.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'reset-password', component: ResetpasswordComponent },
+  { path: 'verify-otp', component: VerifyotpComponent },
+  { path: 'create-new-password', component: CreatenewpasswordComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
